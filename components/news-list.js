c1bd1243f6134addaad2f703564cc5ded7df4a3b
Ulia <lyanusia@gmail.com>
2025-02-03 13:45:26 +0100

    Multiple Path Segments

--- components/news-list.js
+++ components/news-list.js
@@ -5,7 +5,7 @@ export default function NewsList({ news }) {
         <ul className="news-list">
             {news.map(item =>
                 <li key={item.id}>
-                    <Link href={`news/${item.slug}`}>
+                    <Link href={`/news/${item.slug}`}>
                         <img src={`/images/news/${item.image}`} alt={item.title} />
                         <span>{item.title}</span>
                     </Link>
