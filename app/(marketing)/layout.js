dc29bae6403a761a1aaeaf69008776583d7c436c
Ulia <lyanusia@gmail.com>
2025-05-26 15:45:12 +0200

    fetch news from the backend

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,10 +1,12 @@
 import "../globals.css";
 
 export const metadata = {
-    title: 'Next.js Page Routing & Rendering',
-    description: 'Learn how to route to different pages.',
-  };
-   
-  export default function MarketingLayout({ children }) {
-    return <main>{children}</main>;
-  }
+  title: 'Next.js Page Routing & Rendering',
+  description: 'Learn how to route to different pages.',
+};
+
+export default function MarketingLayout({ children }) {
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+}
